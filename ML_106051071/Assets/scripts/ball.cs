41b4d5d677cc8c1d79d50793e753cbef60fa355a
G4daze <55180629+G4daze@users.noreply.github.com>
2020-04-28 16:11:40 +0800

    02

---
+++ ML_106051071/Assets/scripts/ball.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    public static bool complete;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "進球感應")
+        {
+            complete = true;
+        }
+
+
+    }
+
+
+
+}
